a2e77e40e8a99e814782af0fb92701dba65d4e91
konstantinnovation <konstans@stuy.edu>
2023-11-27 08:52:45 -0500

    Update Sorts.java

--- Sorts.java
+++ Sorts.java
@@ -2,5 +2,10 @@ public class Sorts{
   public static void bubblesort(int[]data){
   
   }
+
+  /*Do not implement until directions are posted*/
+  public static void selectionsort(int[]data){}
   
+  /*Do not implement until directions are posted*/
+  public static void insertionsort(int[]data){}
 }
